f4c69a5a0fa03dd679369c3809023ecf2164a634
Brian Weis <brian@brianweis.net>
2017-02-16 21:17:24 -0700

    Test

--- app/index.js
+++ app/index.js
@@ -1,13 +1,18 @@
 // This must be done before you can use any of the fs package functions below
 fs = require('fs');
+var replaceall = require("replaceall");
+//var Regex = require("regex");
 console.log('Transcript Parser!')
+
 
 this.captionContents = "test";
 
 
 fs.readFile('captions.vtt', 'utf8', function (err, fileContents) {
     if (err) throw err;
-    this.captionContents = fileContents;
+    //str.replace(new RegExp(find, 'g'), replace);
+    this.captionContents = replaceall("<", "wg", fileContents);
+     //fileContents.replaceAll('<','g'); //'<[^>]+>';
     fs.writeFile('output.txt', this.captionContents, function (err) {
             if (err) throw err;
         })
